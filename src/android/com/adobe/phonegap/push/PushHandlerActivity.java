864a48542c2f6e47dc30e1742eb8caee5920e1dc
dhirajingole <dhiraj.ingole@webgile.com>
2016-06-16 17:55:44 +0530

    Update PushHandlerActivity.java

--- src/android/com/adobe/phonegap/push/PushHandlerActivity.java
+++ src/android/com/adobe/phonegap/push/PushHandlerActivity.java
@@ -19,6 +19,7 @@ public class PushHandlerActivity extends Activity implements PushConstants {
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        moveTaskToBack(true);//ELLITE
         GCMIntentService gcm = new GCMIntentService();
         int notId = getIntent().getExtras().getInt(NOT_ID, 0);
         Log.d(LOG_TAG, "not id = " + notId);
@@ -27,7 +28,6 @@ public class PushHandlerActivity extends Activity implements PushConstants {
         notificationManager.cancel(GCMIntentService.getAppName(this), notId);
         super.onCreate(savedInstanceState);
         Log.v(LOG_TAG, "onCreate");
-        moveTaskToBack(true);//ELLITE
         String callback = getIntent().getExtras().getString("callback");
         Log.d(LOG_TAG, "callback = " + callback);
         boolean foreground = getIntent().getExtras().getBoolean("foreground", true);
