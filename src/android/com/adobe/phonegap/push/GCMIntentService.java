2a0f1a651663da5191458ecec58dde1a1f3206aa
dhirajingole <dhiraj.ingole@webgile.com>
2016-06-28 10:45:37 +0530

    Update GCMIntentService.java

--- src/android/com/adobe/phonegap/push/GCMIntentService.java
+++ src/android/com/adobe/phonegap/push/GCMIntentService.java
@@ -252,7 +252,7 @@ public class GCMIntentService extends GcmListenerService implements PushConstant
         /*CODE CCUSTOM*/
         Log.d(LOG_TAG, "force launch event");
             Intent intent = new Intent(this, PushHandlerActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             intent.addFlags(Intent.FLAG_FROM_BACKGROUND);
             intent.putExtra(PUSH_BUNDLE, extras);
